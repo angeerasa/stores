743cc2d06930e0f809b875b09cbe0b4558060576
sairamyadhav <sairamyadhav4@gmail.com>
2026-01-08 19:10:45 +0530

    fastapi backend routers and db setup

---
+++ backend/app/db/session.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+from app.core.config import settings
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bing=engine
+)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from routes import api
 
 app = FastAPI()
+
+app.include_router(api.api_router, prefix="/api")
 
 @app.get("/")
 def home():
